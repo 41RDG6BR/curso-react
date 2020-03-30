916e85ebc7e3085134e6a849aaaa16b1f043b73b
Rodrigo Nogueira <rdg6design@gmail.com>
2020-03-30 07:52:35 -0300

    api

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useMongoClient: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio."
+mongoose.Error.messages.Number.min = 
+    "o '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "'{VALUE}' nao é valido para o atributo '{PATH}'."
